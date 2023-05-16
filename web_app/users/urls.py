898c04af6b5cfb23ba495b88a43d11f44d4dd590
Cemal YILMAZ <69977605+cemalyilmaz001@users.noreply.github.com>
2023-05-17 01:00:47 +0300

    eticaret
    .

--- web_app/users/urls.py
+++ web_app/users/urls.py
@@ -5,8 +5,11 @@ from . import views
 app_name = "users"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login/", views.login, name="login"),
-    path("register/", views.register, name="register"),
-    path("mybasket/", views.mybasket, name="mybasket"),
     path("abonelik_save/", views.email_abonelik, name="email_abonelik"),
+
+    path("login/", views.login_view, name="login"),
+    path("register/", views.register, name="register"),
+    path("logout/", views.logout_view, name="logout"),
+
+    path("mybasket/", views.mybasket, name="mybasket"),
 ]
--- web_app/users/views.py
+++ web_app/users/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from .models import Site_Ayarları, Slide_Gösterisi, Email_Abonelik, Ürün_Listesi
 
 site  = Site_Ayarları.objects.all()
@@ -18,27 +21,14 @@ def index(request):
     }
     return render(request, "base.html",context)
 
-def login(request):
-    global site
-    global slide
-    global ürün_list
-    context = {
-        'site': site,
-        'slide':slide,
-        'ürün_list':ürün_list,
-    }
-    return render(request, "login.html",context)
-
-def register(request):
-    global site
-    global slide
-    global ürün_list
-    context = {
-        'site': site,
-        'slide':slide,
-        'ürün_list':ürün_list,
-    }
-    return render(request, "register.html",context)
+def email_abonelik(request):
+    if request.method == 'POST':
+        abone = request.POST["email_abonelik"]
+        email = Email_Abonelik.objects.create(email_address=abone)
+        email.save()
+        return redirect("/")
+    else:
+        return redirect("/")
 
 def mybasket(request):
     global site
@@ -51,11 +41,60 @@ def mybasket(request):
     }
     return render(request, "basket.html",context)
 
-def email_abonelik(request):
-    if request.method == 'POST':
-        abone = request.POST["email_abonelik"]
-        email = Email_Abonelik.objects.create(email_address=abone)
-        email.save()
-        return redirect("/")
+def login_view(request):
+    global site
+    global slide
+    global ürün_list
+
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, f' Hoşgeldiniz {username} !!')
+            return redirect('/')
+        else:
+            messages.info(request, f'Hatalı Giriş Yaptınız !')
     else:
-        return redirect("/")
+        context = {
+            'site': site,
+            'slide':slide,
+            'ürün_list':ürün_list,
+        }
+        return render(request, "login.html",context)
+
+def register(request):
+    global site
+    global slide
+    global ürün_list
+
+    if request.method == 'POST':
+        email           = request.POST['email']
+        username        = request.POST['username']
+        password        = request.POST['password']
+        password_tekrar = request.POST['passwordx']
+        for user in User.objects.all():
+            if user.username == username:
+                messages.success(request, f' Kullanıcı adınız kullanılmakta !!')
+                return redirect('/register')
+
+        if password == password_tekrar:
+            user = User.objects.create_user(f"{str(username)}", f"{str(email)}", f"{str(password)}")
+            user.save()
+            messages.success(request, f' Kulllanıcı Kaydınız Oluşturuldu !!')
+            return redirect('/login')
+        else:
+            messages.success(request, f' Parola Yanlış !!')
+            return redirect('/register')
+    else:
+        context = {
+            'site': site,
+            'slide':slide,
+            'ürün_list':ürün_list,
+        }
+        return render(request, "register.html",context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
